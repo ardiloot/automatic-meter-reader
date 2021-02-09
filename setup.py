7adb7b1ee819369b6510d87b7cd8ce7f1a14cf20
Ardi Loot <ardi.loot@outlook.com>
2021-02-09 22:37:52 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="automatic-meter-reader",
+    version="1.0",
+    url="https://github.com/ardiloot/automatic-meter-reader",
+    author="Ardi Loot",
+    author_email="ardi.loot@outlook.com",
+    packages=["automatic_meter_reader"],
+    zip_safe=False,
+    include_package_data=True
+)
