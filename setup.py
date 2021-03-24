21f0c120679a14888ac6da418c1de089459454d8
Ardi Loot <ardi.loot@outlook.com>
2021-03-24 22:46:48 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="automatic-meter-reader",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/ardiloot/automatic-meter-reader",
     author="Ardi Loot",
     author_email="ardi.loot@outlook.com",
